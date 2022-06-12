d1428c03a5be4c6ac7449e99deae13315004f150
YU-AN CHEN <59487andy@gmail.com>
2022-06-12 18:01:34 +0800

    Update index.js

--- twdh_platform/src/router/index.js
+++ twdh_platform/src/router/index.js
@@ -4,13 +4,15 @@ import portalRoutes from '@/modules/portal/routes'
 import userRoutes from '@/modules/user/routes'
 import opendataRoutes from '@/modules/opendata/routes'
 import tdocuskyRoutes from '@/modules/tdocusky/routes'
+import simpleworkflowRoutes from '@/modules/simpleworkflow/routes'
 
 
 const routes = [
   ...portalRoutes,
   ...userRoutes,
   ...opendataRoutes,
-  ...tdocuskyRoutes
+  ...tdocuskyRoutes,
+  ...simpleworkflowRoutes
 ]
 
 const router = createRouter({
