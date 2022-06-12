89fc9955c9659085a89211f4919c8d153efcb226
Wei-Ji <sckhg1367@gmail.com>
2022-06-13 00:16:21 +0800

    update System.vue with nextPage button.

--- twdh_platform/src/modules/portal/views/System.vue
+++ twdh_platform/src/modules/portal/views/System.vue
@@ -1,6 +1,9 @@
 <template>
   <PageTitle :title="{title: '臺灣史料脈絡分析系統'}" />
   <Material />
+  <div class="button_system_next_div">
+    <button class="button_system_next" @click="nextPage">前往 Open Data 服務 →</button>
+  </div>
 </template>
 
 
@@ -8,12 +11,19 @@
 <script>
 import Material from '../components/Material'
 import PageTitle from '../components/PageTitle'
+
+
 export default {
   name: 'System',
   
   components: {
     Material,
     PageTitle,
-  }
-}
+  },
+  methods:{
+    nextPage(){
+      this.$router.push({ name: this.$router.currentRoute.value.meta.next });
+    },
+  },
+} 
 </script>
