244ecc5192f95499d3538b8066c8915ab0568d85
YU-AN CHEN <59487andy@gmail.com>
2022-06-12 17:59:56 +0800

    Create index.js

---
+++ twdh_platform/src/modules/simpleworkflow/routes/index.js
@@ -0,0 +1,7 @@
+export default [
+  {
+    path: '/simpleWorkFlowIndex',
+    name: 'SimpleWorkFlowIndex',
+    component: () => import(/* webpackChunkName: "simpleWorkFlowIndex" */ '../views/SimpleWorkFlowIndex.vue')
+  },
+]
